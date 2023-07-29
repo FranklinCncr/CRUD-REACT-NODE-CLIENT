d464b37245b33eccbfc6e3c8d7a84d0ad1f248c3
FranklinCncr <franklin.cncr@gmail.com>
2023-07-29 10:36:03 -0500

    Update, delete and sweetalert

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import './App.css';
 import {useState} from 'react';
 import Axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+//sweetalert2
+import Swal from 'sweetalert2';
 
 function App() {
 
   //Estados para inputs
   const [name, setName] = useState('');
   const [country, setCountry] = useState('');
-  const [year, setYear] = useState(0);
+  const [year, setYear] = useState();
+  const [id, setId] = useState();
+
+  //Variable para controlar la edicion
+  const [edit, setEdit] = useState(false);
 
   //Lista vacia users
   const [userList, setUserList] = useState([]);
 
-  //Create user
+  //Crear usuario
   const addUser = () => {
     Axios.post('http://localhost:3001/create',{
       name:name,
@@ -21,9 +27,62 @@ function App() {
       year:year
     }).then(()=>{
       getUser();
-      alert('Usuario registrado!');
+      clean();
+      Swal.fire({
+        title: '<strong>Usuario registrado!</strong>',
+        html: '<i>Bienvenido '+name+'</i>',
+        icon: 'success',
+        timer: 2000
+      })
     });
   }
+
+  //Actualizar Usuario
+  const updateUser = () => {
+    Axios.put('http://localhost:3001/update',{
+      id:id,
+      name:name,
+      country:country,
+      year:year
+    }).then(()=>{
+      getUser();
+      clean();
+      Swal.fire({
+        title: '<strong>Usuario actualizado!</strong>',
+        html: '<i>Se actualizo a '+name+'</i>',
+        icon: 'success',
+        timer: 2000
+      })
+    });
+  }
+
+  //Eliminar usuario
+  const deleteUser = (val) => {
+    Swal.fire({
+      title: '<strong>Eliminar usuario</strong>',
+      html: '<i>Desea eliminar a '+val.name+'?</i>',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si',
+      cancelButtonText: 'No'
+    }).then((result)=>{
+      if(result.isConfirmed){
+        Axios.delete(`http://localhost:3001/delete/${val.id}`).then(()=>{
+          getUser();
+          clean();
+          Swal.fire({
+            title: '<strong>Usuario eliminado</strong>',
+            html: '<i>Se elimino a '+val.name+'</i>',
+            icon: 'success',
+            timer: 2000
+          })          
+        });
+      }
+    })
+    
+  } 
 
   //Obtener lista de usuarios
   const getUser = () => {
@@ -32,13 +91,31 @@ function App() {
     });
   }
 
+  //Funcion para completar el formulario con editar
+  const editUser = (val) => {
+    setEdit(true);
+
+    setName(val.name);
+    setCountry(val.country);
+    setYear(val.year);
+    setId(val.id);
+  }
+
+  const clean = () =>{
+    setId('');
+    setName('');
+    setCountry('');
+    setYear('');
+    setEdit(false);
+  }
+
   //Cargar lista al iniciar
   //getUser();
 
   return (
     <div className='container'>      
 
-      <div className="card text-center">
+      <div className="card text-center mt-2">
 
         <div className="card-header">
           Formulario Usuarios
@@ -48,34 +125,42 @@ function App() {
 
           <div className="input-group mb-3">
             <span className="input-group-text" id="basic-addon1">Nombre</span>
-            <input type="text" onChange={(event)=>{
+            <input type="text" value={name} onChange={(event)=>{
               setName(event.target.value);
             }}  className="form-control" placeholder="Ingrese nombre" aria-label="Username" aria-describedby="basic-addon1"/>
           </div>
 
           <div className="input-group mb-3">
             <span className="input-group-text" id="basic-addon1">Pais</span>
-            <input type="text"  onChange={(event)=>{
+            <input type="text"  value={country} onChange={(event)=>{
               setCountry(event.target.value);
             }} className="form-control" placeholder="Ingrese país" aria-label="Username" aria-describedby="basic-addon1"/>
           </div>
 
           <div className="input-group mb-3">
             <span className="input-group-text" id="basic-addon1">Edad</span>
-            <input type="number" onChange = {(event)=>{
+            <input type="number" value={year} onChange = {(event)=>{
               setYear(event.target.value);
             }} className="form-control" placeholder="Ingrese edad" aria-label="Username" aria-describedby="basic-addon1"/>
           </div>
 
         </div>
         <div className="card-footer text-muted">
-          <button className='btn btn-success' onClick={addUser} >Registrar</button>
+          {
+            edit?
+            <div>
+              <button className='btn btn-success me-1' onClick={updateUser} >Actualizar</button>
+              <button className='btn btn-warning ms-1' onClick={clean} >Cancelar</button>
+            </div>
+            :<button className='btn btn-success' onClick={addUser} >Registrar</button>
+          }
+          
         </div>
       </div>
 
       {/* LISTA DE USARIOS */}       
 
-      <div className="card text-center">
+      <div className="card text-center mt-2">
 
         <div className="card-header">
           Lista de Usuarios
@@ -89,6 +174,7 @@ function App() {
                 <th scope="col">Nombre</th>
                 <th scope="col">País</th>
                 <th scope="col">Edad</th>
+                <th scope="col">Acciones</th>
               </tr>
             </thead>
             <tbody>
@@ -99,6 +185,16 @@ function App() {
                         <td>{val.name}</td>
                         <td>{val.country}</td>
                         <td>{val.year}</td>
+                        <td>
+                          <div class="btn-group" role="group" aria-label="Basic example">
+                            <button type="button" 
+                            onClick={()=>{
+                              editUser(val);
+                            }}  
+                            class="btn btn-info">Editar</button>
+                            <button type="button" onClick={()=>{deleteUser(val);}} class="btn btn-danger">Eliminar</button>
+                          </div>                       
+                        </td>
                       </tr>
               })
               }              
